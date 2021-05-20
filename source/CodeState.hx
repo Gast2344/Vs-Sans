6f340ceb3bb654f392c9f53fd49d2eaddffd031c
Gast <73070724+Gast2344@users.noreply.github.com>
2021-05-21 00:46:24 +0300

    Initial Commit

---
+++ source/CodeState.hx
@@ -0,0 +1,42 @@
+package;
+
+import flixel.FlxG;
+import flixel.FlxSprite;
+import flixel.FlxSubState;
+import flixel.text.FlxText;
+import flixel.util.FlxColor;
+import lime.app.Application;
+
+class CodeState extends MusicBeatState
+{
+	public static var leftState:Bool = false;
+
+	public static var needVer:String = "IDFK LOL";
+
+	override function create()
+	{
+		super.create();
+		var bg:FlxSprite = new FlxSprite().makeGraphic(FlxG.width, FlxG.height, FlxColor.BLACK);
+		add(bg);
+		var txt:FlxText = new FlxText(0, 0, FlxG.width,
+			"Congrats!\n" + "Hold the DJF keys for a surprise :)\n" + "Press ENTER or ESC To go back to menu" , 32);
+		txt.setFormat("VCR OSD Mono", 32, FlxColor.WHITE, CENTER);
+		txt.screenCenter();
+		add(txt);
+	}
+
+	override function update(elapsed:Float)
+	{
+		if (controls.ACCEPT)
+		{
+			leftState = true;
+			FlxG.switchState(new MainMenuState());
+		}
+		if (controls.BACK)
+		{
+			leftState = true;
+			FlxG.switchState(new MainMenuState());
+		}
+		super.update(elapsed);
+	}
+}
